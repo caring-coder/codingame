dbff8890c4d4b8b5494a4796ee05f03d6b568dbb
Joseph Verron <joseph@verron.pro>
2020-06-19 14:30:54 +0200

    last

--- tron-battle.py
+++ tron-battle.py
@@ -1,15 +1,199 @@
 import sys
-import math
+from asyncio import Queue
+from time import perf_counter_ns
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
+FREE = "·"
+WALL = "█"
 
-n = int(input())
-l = int(input())
-for i in range(n):
-    line = input()
+PLAYERS_SYMBOLS = ["o", "+", "x", "¤"]
+HEAD_SYMBOL = "■"
+DEAD = -1, -1
 
-# Write an action using print
-# To debug: print("Debug messages...", file=sys.stderr)
 
-print("2")
+class Game:
+    def __str__(self):
+        representation = ""
+        for row in range(self.height):
+            for col in range(self.width):
+                representation += HEAD_SYMBOL if (row, col) in self.players else self.cell(row, col)
+            representation += "\n"
+        return representation
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __init__(self, width, height, players_starting_positions, current_player):
+        self.width = width
+        self.height = height
+        self.nodes = [[FREE for _ in range(width)] for _ in range(height)]
+        self.players = players_starting_positions
+        for player, position in enumerate(players_starting_positions):
+            self.update(player, *position)
+        self.me = current_player
+
+    def cell(self, row, col):
+        if row < 0:
+            return WALL
+        if row >= self.height:
+            return WALL
+        if col < 0:
+            return WALL
+        if col >= self.width:
+            return WALL
+
+        return self.nodes[row][col]
+
+    def neighbors(self, row, col):
+        if row < 0:
+            return []
+        if row > self.height:
+            return []
+        if col < 0:
+            return []
+        if col > self.width:
+            return []
+
+        directions = [
+            ("RIGHT", 0, 1),
+            ("DOWN", 1, 0),
+            ("LEFT", 0, -1),
+            ("UP", -1, 0)
+        ]
+        return [(direction, row + diff_row, col + diff_col) for direction, diff_row, diff_col in directions]
+
+    def free_neighbors(self, row, col):
+        neighbors = self.neighbors(row, col)
+        free = [neighbor for neighbor in neighbors if self.is_free(neighbor[1], neighbor[2])]
+        return free
+
+    def is_free(self, row, col):
+        cell = self.cell(row, col)
+        return cell != WALL and (cell == FREE or self.players[PLAYERS_SYMBOLS.index(cell)] == DEAD)
+
+    def update(self, player, row, col):
+        last_row, last_col = self.players[player]
+        last_content = self.nodes[row][col]
+
+        if row < 0 or col < 0:
+            self.players[player] = DEAD
+        else:
+            self.players[player] = row, col
+            self.nodes[row][col] = PLAYERS_SYMBOLS[player]
+
+        return last_row, last_col, last_content
+
+    def rollback(self, player, row, col, cell):
+        self.players[player] = row, col
+        self.nodes[row][col] = cell
+
+    def alphabeta(self, player, depth, timer, alpha, beta):
+        current_cell = self.players[player]
+        possible_moves = self.free_neighbors(*current_cell)
+        next_player = (player + 1) % len(self.players)
+
+        if depth <= 0 or perf_counter_ns() - timer > 95000:
+            valuation = self.valuation()
+            return valuation, "TOO DEEP"
+
+        if not possible_moves:
+            last_row, last_col, last_content = self.update(player, -1, -1)
+            move_value, _ = self.alphabeta(next_player, depth - 1, timer, alpha, beta)
+            self.rollback(player, last_row, last_col, last_content)
+            return move_value, "DEAD PERSON"
+
+        maximising = player == self.me
+        if maximising:
+            value = -2
+            best_direction = "GIVE UP"
+            for direction, row, col in possible_moves:
+
+                last_row, last_col, last_content = self.update(player, row, col)
+                move_value, _ = self.alphabeta(next_player, depth - len(possible_moves), timer, alpha, beta)
+                self.rollback(player, last_row, last_col, last_content)
+
+                if move_value > value:
+                    value = move_value
+                    alpha = move_value
+                    best_direction = direction
+                if alpha > beta:
+                    break
+        else:
+            value = 2
+            best_direction = "GIVE UP"
+            for direction, row, col in possible_moves:
+
+                last_row, last_col, last_content = self.update(player, row, col)
+                move_value, _ = self.alphabeta(next_player, depth - 1, timer, alpha, beta)
+                self.rollback(player, last_row, last_col, last_content)
+
+                if move_value < value:
+                    value = move_value
+                    beta = move_value
+                    best_direction = direction
+                if alpha > beta:
+                    break
+        return value, best_direction
+
+    def valuation(self):
+        my_value = 0
+        enemy_value = 0
+        queues = list()
+        positions = list(filter(lambda player: player != DEAD, self.players))
+
+        saved_nodes = [[content for content in row] for row in self.nodes]
+
+        for cell in positions:
+            queue = Queue()
+            queue.put_nowait(cell)
+            queues.append(queue)
+
+        while not all(map(lambda queue: queue.empty(), queues)):
+            for player, queue in enumerate(queues):
+                if queue.empty():
+                    continue
+                if self.players[player][0] == -1:
+                    item_row, item_col = queue.get_nowait()
+                    self.nodes[item_row][item_col] = FREE
+                    continue
+                symbol = PLAYERS_SYMBOLS[player]
+                item_row, item_col = queue.get_nowait()
+                for _, neighbor_row, neighbor_col in self.free_neighbors(item_row, item_col):
+                    self.nodes[neighbor_row][neighbor_col] = symbol
+
+                    queue.put_nowait((neighbor_row, neighbor_col))
+                    if player == self.me:
+                        my_value += 1
+                    else:
+                        enemy_value += 1
+
+        self.nodes = saved_nodes
+        return (my_value - enemy_value) / (my_value + enemy_value + 1)
+
+    def running(self):
+        return any(map(lambda player: player[0] > 0, self.players))
+
+
+def parse_input():
+    while True:
+        nb_players, player_index = [int(i) for i in input().split()]
+        for player in range(nb_players):
+            _, _, col, row = [int(i) for i in input().split()]
+            print((player, row, col), file=sys.stderr)
+            yield player, row, col
+
+
+nb_players, player_index = [int(i) for i in input().split()]
+players = []
+for player in range(nb_players):
+    col, row, _, _ = [int(i) for i in input().split()]
+    players.append((row, col))
+
+game = Game(30, 20, players, player_index)
+
+inputs = parse_input()
+while True:
+    value, direction = game.alphabeta(player_index, 25, perf_counter_ns(), -2, 2)
+    print(direction)
+    for player in range(nb_players):
+        game.update(*next(inputs))
+    print(game, file=sys.stderr)
--- unleash-the-geek-amadeus.py
+++ unleash-the-geek-amadeus.py
@@ -21,12 +21,21 @@ class Pos:
     def distance(self, pos):
         return abs(self.x - pos.x) + abs(self.y - pos.y)
 
+    def __str__(self):
+        return str((self.x, self.y))
+
 
 class Entity(Pos):
     def __init__(self, x, y, type, id):
         super().__init__(x, y)
         self.type = type
         self.id = id
+
+    def __str__(self):
+        return "{}:[{},{}]".format(self.id, self.type, super().__str__())
+
+    def __repr__(self):
+        return "{}:[{},{}]".format(self.id, self.type, super().__str__())
 
 
 class Robot(Entity):
@@ -40,11 +49,17 @@ class Robot(Entity):
     def move(self, x, y, message=""):
         print(f"MOVE {x} {y} {message}")
 
+    def move_vec(self, vector, message=""):
+        self.move(self.x + vector[0], self.y + vector[1], message)
+
     def wait(self, message=""):
         print(f"WAIT {message}")
 
     def dig(self, x, y, message=""):
         print(f"DIG {x} {y} {message}")
+
+    def dig_vec(self, vector, message=""):
+        self.dig(self.x + vector[0], self.y + vector[1], message)
 
     def request(self, requested_item, message=""):
         if requested_item == RADAR:
@@ -60,7 +75,11 @@ class Cell(Pos):
         super().__init__(x, y)
         self.amadeusium = amadeusium
         self.hole = hole
-        self.potential_trap = False
+        self._potential_trap = False
+
+    def set_potential_trap(self):
+        if self.hole:
+            self._potential_trap = True
 
     def has_hole(self):
         return self.hole == HOLE
@@ -69,6 +88,11 @@ class Cell(Pos):
         self.amadeusium = amadeusium
         self.hole = hole
 
+    def __str__(self):
+        return "cell:[{},{},{}]".format(self.amadeusium, self.hole, super().__str__())
+
+    def __repr__(self):
+        return "cell:[{},{},{}]".format(self.amadeusium, self.hole, super().__str__())
 
 class Grid:
     def __init__(self):
@@ -83,11 +107,9 @@ class Grid:
         return Cell(x, y, 0, 0)
 
 
-my_trap_spots = list(range(2, height - 2))
-
 class Game:
     def update_entity(self, id, type, x, y, item):
-        print("update_entity", file=sys.stderr)
+        entity = Entity(x, y, type, id)
         if type == ROBOT_ALLY:
             self.my_robots.append(Robot(x, y, type, id, item))
         elif type == ROBOT_ENEMY:
@@ -95,19 +117,20 @@ class Game:
             if self.enemy_history[id]:
                 previous = self.enemy_history[id][0]
                 if previous.x == current.x and previous.y == current.y:
-                    self.grid.get_cell(current.x, current.y).potential_trap = True
-                    self.grid.get_cell(current.x + 1, current.y).potential_trap = True
-                    self.grid.get_cell(current.x, current.y + 1).potential_trap = True
-                    self.grid.get_cell(current.x - 1, current.y).potential_trap = True
-                    self.grid.get_cell(current.x, current.y + 1).potential_trap = True
+                    self.grid.get_cell(current.x, current.y).set_potential_trap()
+                    self.grid.get_cell(current.x + 1, current.y).set_potential_trap()
+                    self.grid.get_cell(current.x, current.y + 1).set_potential_trap()
+                    self.grid.get_cell(current.x - 1, current.y).set_potential_trap()
+                    self.grid.get_cell(current.x, current.y + 1).set_potential_trap()
             self.enemy_history[id].insert(0, current)
         elif type == TRAP:
-            self.traps.append(Entity(x, y, type, id))
+            print(entity, file=sys.stderr)
+            self.traps.append(entity)
         elif type == RADAR:
-            self.radars.append(Entity(x, y, type, id))
+            self.radars.append(entity)
 
-    def __init__(self, turns):
-        self.turns_left = turns
+    def __init__(self):
+        self.turn = 0
         self.grid = Grid()
         self.my_score = 0
         self.enemy_score = 0
@@ -123,19 +146,17 @@ class Game:
 
     def first_radar_spot(self):
         for n in range(5, width-2):
-            for m in range(2, height-2):
+            for m in range(3, height-3):
                 cell = self.grid.get_cell(n, m)
+                if self.is_trap(cell):
+                    continue
+                if cell._potential_trap:
+                    continue
                 radar_distances = list(map(lambda radar: cell.distance(radar), self.radars))
                 if not radar_distances:
                     return cell
-                elif min(radar_distances) > 5:
+                elif min(radar_distances) > 6:
                     return cell
-        return None
-
-    def first_trap_spot(self):
-        for n in my_trap_spots:
-            if not self.is_trap(self.grid.get_cell(1, n)):
-                return Pos(1, n)
         return None
 
     def is_trap(self, cell):
@@ -162,36 +183,42 @@ class Game:
         else:
             return enemy_robots_in_range / my_robots_in_range >= ratio
 
-    def near_trap(self, cell):
+    def near_traps(self, cell, distance=1):
         if not self.traps:
-            return None
-        trap_distances = list(map(lambda trap: trap.distance(cell), self.traps))
-        return self.traps[trap_distances.index(min(trap_distances))]
+            return []
+        near_traps = list(filter(lambda trap: trap.distance(cell) <= distance, self.traps))
+        return near_traps
 
     def winning(self):
-        # return True
         bot_valuation = lambda robot: 0 if robot.is_dead() else 1
         nb_my_robots = sum(map(bot_valuation, self.my_robots))
-        nb_enemy_robots = sum(map(bot_valuation, [value[0] for value in self.enemy_history.values()]))
+        nb_enemy_robots = sum(map(bot_valuation, self.enemy_robots()))
         if not nb_my_robots:
             return False
-        return (self.my_score + (nb_my_robots * self.turns_left / 10)) - (self.enemy_score + (nb_enemy_robots * self.turns_left / 10)) > (self.turns_left / 20)
+        return (self.my_score + (nb_my_robots * 2)) > (self.enemy_score + (nb_enemy_robots * 2) + 10)
 
     def nearest_amadeusium_spot(self, position):
+        print(self.safe_amadeusium_spots, file=sys.stderr)
+        print(self.unsafe_amadeusium_spots, file=sys.stderr)
         safe_amadeusium_spots_distances = list(map(lambda spot: position.distance(spot), self.safe_amadeusium_spots))
         unsafe_amadeusium_spots_distances = list(map(lambda spot: position.distance(spot), self.unsafe_amadeusium_spots))
+
+        spots = [None, None]
         if safe_amadeusium_spots_distances:
-            return self.safe_amadeusium_spots[safe_amadeusium_spots_distances.index(min(safe_amadeusium_spots_distances))]
-        elif unsafe_amadeusium_spots_distances:
-            return self.unsafe_amadeusium_spots[unsafe_amadeusium_spots_distances.index(min(unsafe_amadeusium_spots_distances))]
-        else:
-            return None
+            spots[0] = self.safe_amadeusium_spots[safe_amadeusium_spots_distances.index(min(safe_amadeusium_spots_distances))]
+
+        if unsafe_amadeusium_spots_distances:
+            spots[1] = self.unsafe_amadeusium_spots[unsafe_amadeusium_spots_distances.index(min(unsafe_amadeusium_spots_distances))]
+
+        return spots
 
     def near_HQ(self, robot):
         return robot.x == min(map(lambda robot: robot.x, filter(lambda robot: not robot.is_dead(), self.my_robots)))
 
+    def enemy_robots(self):
+        return [value[0] for value in self.enemy_history.values()]
+
     def next_turn(self):
-        print("next_turn", file=sys.stderr)
         self.radars = []
         self.traps = []
         self.my_robots = []
@@ -200,65 +227,88 @@ class Game:
         self.seeking_trap = False
         self.awaiting_trap = False
 
-        self.turns_left -= 1
+        self.turn += 1
 
+    def compute_strat(self):
         cells = [self.grid.get_cell(n, m) for n in range(width) for m in range(height)]
-        amadeusium_spots = list(filter(lambda cell: cell.amadeusium != "?" and int(cell.amadeusium) > 0 , cells))
-        self.safe_amadeusium_spots = list(filter(lambda cell: not cell.potential_trap, amadeusium_spots))
-        self.unsafe_amadeusium_spots = list(filter(lambda cell: cell.potential_trap, amadeusium_spots))
-        print("next_turn end", file=sys.stderr)
+        amadeusium_spots = list(
+            filter(lambda cell: cell.amadeusium != "?" and int(cell.amadeusium) > 0 and not self.is_trap(cell), cells))
+        self.safe_amadeusium_spots = list(filter(lambda cell: not cell._potential_trap, amadeusium_spots))
+        self.unsafe_amadeusium_spots = list(filter(lambda cell: cell._potential_trap, amadeusium_spots))
+        print("traps " + str(self.traps), file=sys.stderr)
+        print("next turn " + str(amadeusium_spots), file=sys.stderr)
+        print("next turn " + str(self.safe_amadeusium_spots), file=sys.stderr)
+        print("next turn " + str(self.unsafe_amadeusium_spots), file=sys.stderr)
+
 
 
 def apply_strategy(game, robot):
-    print("apply_strategy", file=sys.stderr)
     if robot.is_dead():
         robot.wait("Dead!")
         return
 
-    next_cell = game.grid.get_cell(robot.x + 1, robot.y)
-    radar_spot = game.first_radar_spot()
-    trap_spot = game.first_trap_spot()
-    near_trap = game.near_trap(robot)
-    winning = game.winning()
-    amadeusium_spot = game.nearest_amadeusium_spot(robot)
-    near_HQ = game.near_HQ(robot)
-    if robot.item == AMADEUSIUM:
-        robot.move(0, robot.y)
-    elif not trap_spot \
-            and not game.awaiting_trap \
-            and game.ratio_mine_vs_them_is_over(1 if winning else 2) \
-            and robot.distance(near_trap) < 2:
-        robot.dig(near_trap.x, near_trap.y, "EXPLODE")
-        game.awaiting_trap = True
-    elif robot.item == RADAR and radar_spot:
-        robot.dig(radar_spot.x + robot.id % 2, radar_spot.y + robot.id // 2 % 2)
-    elif robot.item == TRAP and trap_spot:
-        robot.dig(trap_spot.x, trap_spot.y)
+    ending = game.turn > 122
+    beginning = game.turn < 66
 
-    elif radar_spot and game.radar_cooldown == 0 and not game.seeking_radar and robot.item == NONE and near_HQ:
+    winning = game.winning()
+    safe_amadeusium, unsafe_amadeusium = game.nearest_amadeusium_spot(robot)
+    safe_amadeusium, unsafe_amadeusium = safe_amadeusium, None if beginning else unsafe_amadeusium
+    if safe_amadeusium:
+        game.safe_amadeusium_spots.remove(safe_amadeusium)
+    if unsafe_amadeusium:
+        game.unsafe_amadeusium_spots.remove(unsafe_amadeusium)
+    radar_spot = game.first_radar_spot() or safe_amadeusium or unsafe_amadeusium
+    amadeusium = safe_amadeusium or unsafe_amadeusium
+    near_HQ = game.near_HQ(robot)
+    near_traps = game.near_traps(robot)
+    opportunity_trap = find_opportunity_trap(game, near_traps, robot, unsafe_amadeusium)
+
+
+    if robot.item == AMADEUSIUM:
+        robot.move(0, robot.y, "Back to HQ")
+    elif opportunity_trap:
+        robot.dig(opportunity_trap.x, opportunity_trap.y, "Opportunity")
+    elif robot.item == RADAR and radar_spot and len(game.safe_amadeusium_spots) < 5:
+        robot.dig(radar_spot.x, radar_spot.y, "Put Standard Radar")
+    elif robot.item == RADAR and amadeusium and len(game.safe_amadeusium_spots) >= 5:
+        robot.dig(amadeusium.x, amadeusium.y, "Put Radar Wherever")
+    elif robot.item == TRAP and amadeusium:
+        robot.dig(amadeusium.x, amadeusium.y, "Put Trap on Amadeusium")
+    elif radar_spot and game.radar_cooldown == 0 and not game.seeking_radar and robot.item == NONE and near_HQ and len(game.safe_amadeusium_spots) < 8:
         robot.request(RADAR, "Request Radar")
         game.seeking_radar = True
-    elif not winning and trap_spot and game.trap_cooldown == 0 and not game.seeking_trap and robot.item == NONE and near_HQ:
+    elif game.radar_cooldown == 0 and not game.seeking_radar and robot.x == 0 and len(game.safe_amadeusium_spots) >= 5:
+        robot.request(RADAR, "Request Radar")
+        game.seeking_radar = True
+    elif not ending and not winning and safe_amadeusium and game.trap_cooldown == 0 and not game.seeking_trap and robot.item == NONE and robot.x == 0:
         robot.request(TRAP, "Request Trap")
         game.seeking_trap = True
-    elif not winning and not trap_spot and not game.awaiting_trap and near_trap and robot.distance(near_trap) > 1:
-        robot.move(near_trap.x - 1, near_trap.y, "Approach trap")
-        game.awaiting_trap = True
-    elif near_trap and robot.distance(near_trap) <= 1 and not game.awaiting_trap and not game.ratio_mine_vs_them_is_over(1 if winning else 2):
-        robot.wait("Waiting for enemies")
-        game.awaiting_trap = True
-    elif amadeusium_spot:
-        robot.dig(amadeusium_spot.x, amadeusium_spot.y, "Targeted digging")
-    elif next_cell and not next_cell.has_hole():
-        robot.dig(robot.x + 1, robot.y, "Blind digging")
+    elif amadeusium:
+        robot.dig(amadeusium.x, amadeusium.y, "Safish Dig")
     else:
-        robot.wait("Lost")
+        robot.wait("...")
 
 
-game = Game(200)
+def find_opportunity_trap(game, near_traps, robot, unsafe_amadeusium):
+    opportunity_trap = None
+    opportunity_trap_value = 0
+    for trap in near_traps:
+        enemy_value = len(list(filter(lambda robot: robot.distance(trap) <= 1, game.enemy_robots())))
+        my_value = len(list(filter(lambda robot: robot.distance(trap) <= 1, game.my_robots)))
+        if (enemy_value - my_value) > opportunity_trap_value:
+            opportunity_trap = trap
+            opportunity_trap_value = enemy_value - my_value
+    if unsafe_amadeusium and robot.distance(unsafe_amadeusium) <= 1:
+        enemy_value = len(list(filter(lambda robot: robot.distance(unsafe_amadeusium) <= 1, game.enemy_robots())))
+        my_value = len(list(filter(lambda robot: robot.distance(unsafe_amadeusium) <= 1, game.my_robots)))
+        if (enemy_value - my_value) > opportunity_trap_value:
+            opportunity_trap = unsafe_amadeusium
+    return opportunity_trap
+
+
+game = Game()
 
 while True:
-    print("main_loop", file=sys.stderr)
     game.my_score, game.enemy_score = [int(i) for i in input().split()]
     for i in range(height):
         inputs = input().split()
@@ -271,7 +321,33 @@ while True:
     for i in range(entity_count):
         id, type, x, y, item = [int(j) for j in input().split()]
         game.update_entity(id, type, x, y, item)
+    game.compute_strat()
 
-    for robot in game.my_robots:
-        print("robot_loop", file=sys.stderr)
-        apply_strategy(game, robot)
+    if game.turn == 1:
+        random.shuffle(game.my_robots)
+        game.my_robots[0].request(RADAR)
+        game.my_robots[1].request(TRAP)
+        game.my_robots[2].wait()
+        game.my_robots[3].wait()
+        game.my_robots[4].wait()
+    elif game.turn == 2:
+        game.my_robots[0].move_vec((+4, 0))
+        game.my_robots[1].move_vec((+4, 0))
+        game.my_robots[2].move_vec((+4, 0))
+        game.my_robots[3].move_vec((+4, 0))
+        game.my_robots[4].move_vec((+4, 0))
+    elif game.turn == 3:
+        game.my_robots[0].dig_vec((+1, 0))
+        game.my_robots[1].dig_vec((+1, 0))
+        game.my_robots[2].dig_vec((+1, 0))
+        game.my_robots[3].dig_vec((+1, 0))
+        game.my_robots[4].dig_vec((+1, 0))
+    elif game.turn == 4:
+        game.my_robots[0].move_vec((-4, 0))
+        game.my_robots[1].move_vec((-4, 0))
+        game.my_robots[2].move_vec((-4, 0))
+        game.my_robots[3].move_vec((-4, 0))
+        game.my_robots[4].move_vec((-4, 0))
+    else:
+        for robot in game.my_robots:
+            apply_strategy(game, robot)
